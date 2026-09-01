4583733a6c7680afdcf71ef5421cc25efdf2ebc3
Rhoanne Nicole Antonio <antonio_rhoannenicole@plpasig.edu.ph>
2026-09-01 21:24:41 +0800

    api fix

--- PLink/src/api.jsx
+++ PLink/src/api.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: { Accept: 'application/json' },
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('ACCESS_TOKEN');
   if (token) config.headers.Authorization = `Bearer ${token}`;
